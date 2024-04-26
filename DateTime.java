aa04f23c157794c2c702d5b6faa508f0982a4c93
arpit <85329366+arpityadav12@users.noreply.github.com>
2024-04-26 19:38:31 +0530

    Add files via upload

---
+++ DateTime.java
@@ -0,0 +1,8 @@
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+public class DateTime {
+    public static void main(String[] args) {
+        System.out.println("CURRENT LOCAL DATE IS :"+LocalDate.now());
+        System.out.println("CURRENT LOCAL TIME IS :"+ LocalTime.now());
+    }
+}
